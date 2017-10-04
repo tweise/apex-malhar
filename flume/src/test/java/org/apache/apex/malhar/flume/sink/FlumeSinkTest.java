ee90c6b98ef7167f78fc5a52cff3ebacaf247db5
Ananth <ananthg.apex@gmail.com>
2017-10-05 05:16:59 +1100

    APEXMALHAR-2546.Mocking-dependencies-should-be-part-of-parent-pom

--- flume/src/test/java/org/apache/apex/malhar/flume/sink/FlumeSinkTest.java
+++ flume/src/test/java/org/apache/apex/malhar/flume/sink/FlumeSinkTest.java
@@ -91,8 +91,6 @@ public class FlumeSinkTest
       }
 
     };
-    DefaultEventLoop eventloop = new DefaultEventLoop("Eventloop-TestClient");
-    eventloop.start();
     FlumeSink sink = new FlumeSink();
     sink.setName("TeskSink");
     sink.setHostname(hostname);
@@ -139,6 +137,8 @@ public class FlumeSinkTest
       }
 
     };
+    DefaultEventLoop eventloop = new DefaultEventLoop("Eventloop-TestClient");
+    eventloop.start();
     discovery.discover();
     try {
       eventloop.connect(new InetSocketAddress(hostname, port), client);
